d141d614c67c49ef7ee3d0f21652379a280a3e4a
Massimiliano Iacono <massimiliano.iacono@iit.it>
2021-08-17 14:04:50 +0000

    Fixed bug event decoding

--- main.py
+++ main.py
@@ -4,29 +4,26 @@ import os
 import json
 from tqdm import tqdm
 from bimvee.exportIitYarp import encodeEvents24Bit
-data_dir = '/mnt/Shared/'
-
-path = os.path.join(data_dir, 'tuna')
+import argparse
 
 
-if __name__ == '__main__':
-
+def frame2events(input_path, output_path):
     numBins = 10
-    step = 1 / (numBins)
-    dirList = os.listdir(path)
+    step = 0.5 / (numBins)
+    dirList = os.listdir(input_path)
     prev_ts = None
     events = []
-    ev_img_list = [x for x in dirList if x.__contains__('ec') and os.path.splitext(x)[-1] == '.png']
+    ev_img_list = sorted([x for x in dirList if x.__contains__('ec') and os.path.splitext(x)[-1] == '.png'])
 
-    for file in tqdm(sorted(ev_img_list)):
-        with open(os.path.join(path, file.split('.')[0] + '.json'), 'r') as jsonFile:
+    for file in tqdm(ev_img_list):
+        with open(os.path.join(input_path, file.split('.')[0] + '.json'), 'r') as jsonFile:
             metadata = json.load(jsonFile)
         timestamp = metadata['timestamp']
         if prev_ts is None:
             prev_ts = timestamp
             continue
-        image = plt.imread(os.path.join(path, file))
-        vCount = (np.round(image / step) - numBins / 2).astype(int)
+        image = plt.imread(os.path.join(input_path, file))
+        vCount = np.round(image / step - numBins).astype(int)
         vIndices = vCount.nonzero()
         for y, x in zip(vIndices[0], vIndices[1]):
             num_events = vCount[y, x]
@@ -41,6 +38,26 @@ if __name__ == '__main__':
     prev_ts = events[0, 2]
 
     # dataDict = {'x': events[:, 0], 'y': events[:, 1], 'ts': events[:, 2], 'pol': events[:, 3].astype(bool)}
-    encodedData = np.array(encodeEvents24Bit(events[:, 2] - events[0, 2], events[:, 0], events[:, 1], events[:, 3].astype(bool))).astype(np.uint32)
-    with open(os.path.join(os.getenv('HOME'), 'Desktop', 'test', 'binaryevents.log'), 'wb') as f:
+    encodedData = np.array(
+        encodeEvents24Bit(events[:, 2] - events[0, 2],
+                          events[:, 0],
+                          events[:, 1],
+                          events[:, 3].astype(bool))).astype(np.uint32)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    with open(os.path.join(output_path, 'binaryevents.log'), 'wb') as f:
         encodedData.tofile(f)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract binary events from sequence of frames')
+    parser.add_argument('--input', '-i', dest='input_path', type=str, required=True,
+                        help='Path to input file')
+    parser.add_argument('--output', '-o', dest='output_path', type=str, required=True,
+                        help='Path to output file')
+    args = parser.parse_args()
+
+    input_path = args.input_path
+    output_path = args.output_path
+
+    frame2events(input_path, output_path)
